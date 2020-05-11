cbce4cedd3e6d6435bce2985fd55725f2a69a4e3
Cherise Knox <cheriseknox@gmail.com>
2020-05-11 16:37:40 -0400

    added contact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,22 @@
+import React from 'react'; 
+
+function Contact() {
+  return (
+    <footer>
+        <div class="contact-container">
+          <a name="contact" class="link-container"></a>
+          <a href="mailto:cheriseknox@gmail.com" class="contact">
+            <i class="fas fa-envelope contact-icon"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/cherise-knox" class="contact">
+          <i class="fab fa-linkedin-in contact-icon"></i>
+          </a>
+          <a href="https://github.com/cherisek27" class="contact"> 
+            <i class="fab fa-github contact-icon"></i>
+          </a>
+        </div>
+    </footer>
+  )
+}
+
+export default Contact; 
