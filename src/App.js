c771897ece63a52b8fc3c6e34fdec41beaa13170
Cherise Knox <cheriseknox@gmail.com>
2020-05-11 16:34:40 -0400

    added about, projects, and skills

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="layout-container">
       <Header />
+      <section className="main-content">
+        <div className="content">
+          <About />
+          <Skills />
+          <Projects />
+        </div>
+      </section>
     </div>
   );
 }
