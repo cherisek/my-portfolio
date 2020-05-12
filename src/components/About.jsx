df1a3a64ac5554b303fe974e5ce23b22bc46fe80
Cherise Knox <cheriseknox@gmail.com>
2020-05-11 22:15:23 -0400

    updated about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,12 +8,19 @@ function About() {
       <a href="/" name="about" className="link-container">
         <h2>About Me</h2>
         <p className="about-me-description">
-            Coming from a marketing background, I started my web development journey in 2017 and I have been enjoying
-            growing and learning throughout this journey. There is never a dull moment
-            within development. I am evolving on my design side and always trying to learn 
-            or add on to my current knowledge. Traveling, running, and spending time with 
-            family and frisends keep my balance between web development.
+          Known for my professionalism, dedication, integrity, and passion.  I'm skilled in developing web designs, testing, and 
+          implementing clean code into production.
         </p>
+        <h3>Summary of Qualifications:</h3>
+        <ul className="list">
+          <li>Exceptionally well organized, great communicator, and highly motivated</li>
+          <li>Highly effective in developing and maintaining a positive and productive work environment</li>
+          <li>Effective leader; able to prioritize my (as wells as the team) workload and motivate others</li>
+          <li>Detailed oriented and skilled in setting priorities</li>
+          <li>Work creatively and effectively with peers and management</li>
+          <li>Work well independently and as member of a team</li>
+          <li>Able to plan, develop, test, and implement activities on time</li>
+        </ul>
       </a>
       <div className="border-bottom"></div>
     </>
